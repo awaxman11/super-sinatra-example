b3d97bc383245c51d907b4c8805c42a25b4ecc06
awaxman11 <awaxman11@gmail.com>
2013-06-27 17:13:50 -0400

    first commit

--- app.rb
+++ app.rb
@@ -4,7 +4,18 @@ module SuperSinatra
   class App < Sinatra::Base
 
     get '/' do
+      erb :'super_hero'
     end
+
+    post '/team' do
+      @team = params[:team]
+      @members = params[:team][:members]
+      erb :'team'
+    end
+
+    get '/team' do
+      erb :'team'
+    end  
 
   end
 end
